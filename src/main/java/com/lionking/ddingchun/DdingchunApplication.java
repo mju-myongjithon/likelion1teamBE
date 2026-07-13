8f3670f358d29275da637e5e728fbe88fb1506cd
장선호 <tmmjang@naver.com>
2026-07-13 19:29:01 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/lionking/ddingchun/DdingchunApplication.java
@@ -0,0 +1,13 @@
+package com.lionking.ddingchun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdingchunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdingchunApplication.class, args);
+	}
+
+}
